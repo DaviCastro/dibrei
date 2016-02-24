bc132d4eec3222b7670320a8a0fd08c83f173e0b
Fernandes <Filipe.Fernandes@unisys.com>
2016-02-24 17:01:00 -0300

    teste

--- front-end/www/js/controllers.js
+++ front-end/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', ['uiGmapgoogle-maps'])
+angular.module('app.controllers', [])
   
 .controller('loginCtrl', function($scope) {
 
@@ -29,16 +29,6 @@ var s = this;
 })
    
 .controller('listaPresencaCtrl', function($scope) {
-<<<<<<< HEAD
-=======
-	this.aba = 1;
-	this.mudarAba = function(abaEscolhida) {
-		this.aba = abaEscolhida;
-	};
-	this.mostrarAba = function(abaAtual) {
-		return this.aba === abaAtual;
-	};
->>>>>>> a60313ac5fc331456bbd78a1ed1d10b35e2a6ad9
 })
    
 .controller('avulsoCtrl', function($scope) {
