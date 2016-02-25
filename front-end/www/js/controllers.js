068648d90b3dfa58515b97154040c6b803c4f376
Fernandes <Filipe.Fernandes@unisys.com>
2016-02-25 09:48:39 -0300

    Go

--- front-end/www/js/controllers.js
+++ front-end/www/js/controllers.js
@@ -28,7 +28,36 @@ var s = this;
 
 })
    
-.controller('listaPresencaCtrl', function($scope) {
+.controller('listaPresencaCtrl', function($scope, $ionicPopup, $timeout) {
+	$scope.removerDibreiro = function() {
+	   var confirmPopup = $ionicPopup.confirm({
+	     title: 'Remover Dibrei',
+	     template: 'Você tem certeza que deseja desconfirmar esse dibreiro?',
+	     cancelText: 'Não',
+	     okText: 'Sim'
+	   });
+
+	   confirmPopup.then(function(res) {
+	     if(res) {
+	       console.log('Remover');
+	     }
+	   });
+	 };
+
+	 $scope.confirmarDibreiro = function() {
+	   var confirmPopup = $ionicPopup.confirm({
+	     title: 'Confirmar Dibreiro',
+	     template: 'Você tem certeza que deseja confirmar esse dibreiro?',
+	     cancelText: 'Não',
+	     okText: 'Sim'
+	   });
+
+	   confirmPopup.then(function(res) {
+	     if(res) {
+	       console.log('Adicionar');
+	     }
+	   });
+	 };
 })
    
 .controller('avulsoCtrl', function($scope) {
