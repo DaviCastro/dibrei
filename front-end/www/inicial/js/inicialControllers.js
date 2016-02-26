439690d50da5967194b92c1f1cb1231ad61baddb
Reis <Otavio.Reis@unisys.com>
2016-02-26 11:34:04 -0300

    Efeitos na lista usando o recurso nativo do ionic

--- front-end/www/inicial/js/inicialControllers.js
+++ front-end/www/inicial/js/inicialControllers.js
@@ -2,8 +2,20 @@
 	var app = angular.module('app.inicial', []);
 
 
-	app.controller('inicialCtrl', ['$ionicLoading', function ($ionicLoading){
+	app.controller('inicialCtrl', function ($timeout, ionicMaterialInk, ionicMaterialMotion){
 		
-	}]);
+
+	 // Delay expansion
+    $timeout(function() {
+        
+    }, 300);
+
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+
+	});
 
 })();
